364218c354efd4ca1271b4a09ad2343f067f3fda
Nitish sharma <sharmanitish183@gmail.com>
2024-01-05 16:36:46 +0530

    added user section

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,13 +1,71 @@
-import React from 'react'
 
+import '../../normy.css'
 import SideNav from './SideNav'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 export default function Home() {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+      axios.get('http://localhost:3001')
+          .then(result => setUsers(result.data))
+          .catch(err => console.log(err))
+  }, [])
+
+  const handleDelete = (id) => {
+      axios.delete('http://localhost:3001/deleteUser/' + id)
+          .then(res => {
+              console.log(res)
+              window.location.reload()
+          })
+          .catch(errr => console.log(errr))
+  }
   return (
-    <div>
+    <div className='d-flex'>
       
        <SideNav/>
-        
+       <div className='d-flex vh-100 vw-100 justify-content-center bg-primary  align-items-center'>
+            <div className='w-50 bg-white rounded p-3'>
+                <Link to='/create' className='btn btn-success'>Add +</Link>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Age</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user) => {
+
+                                return <tr>
+                                    <td>
+                                        {user.name}
+                                    </td>
+                                    <td>
+                                        {user.email}
+                                    </td>
+                                    <td>
+                                        {user.age}
+                                    </td>
+                                    <td>
+                                        <Link to={`/update/${user._id}`} className='btn btn-success'>Update</Link>
+                                        <button className='btn btn-danger'
+                                            onClick={(e) => handleDelete(user._id)}>Delete</button>
+                                    </td>
+                                </tr>
+                            })
+                        }
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
     </div>
   )
 }
--- client/src/components/Home/SideNav.js
+++ client/src/components/Home/SideNav.js
@@ -1,40 +1,43 @@
 import React from 'react'
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { NavLink } from 'react-router-dom';
+
 export default function sideNavbar() {
   return (
     <div className="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark vh-100" style={{width: "280px"}}>
-    <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-      
-      <span className="fs-4">Sidebar</span>
-    </a>
+    <NavLink exact to='/home' className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+    <i class="bi bi-person-rolodex forSize"></i>
+      <span className="fs-4">Contact Manager</span>
+    </NavLink>
     <hr/>
     <ul className="nav nav-pills flex-column mb-auto">
       <li className="nav-item">
-        <a href="#" className="nav-link active" aria-current="page">
-        <i className="bi bi-house-door-fill"></i>
+        <NavLink to='/home' className="nav-link active" aria-current="page">
+        <i class="bi bi-house-door icon-space"></i>
           Home
-        </a>
+        </NavLink>
       </li>
       <li>
-        <a href="#" className="nav-link text-white">
-        <i className="bi bi-speedometer2"></i>
-          Dashboard
-        </a>
+        <NavLink to='/create' className="nav-link text-white">
+        <i className="bi bi-speedometer2 icon-space"></i>
+          Add New Contact
+        </NavLink>
       </li>
       <li>
-        <a href="#" className="nav-link text-white">
-        <i className="bi bi-speedometer2"></i>
-          Dashboard
-        </a>
+        <NavLink to='/create' className="nav-link text-white">
+        <i className="bi bi-speedometer2 icon-space"></i>
+          Add New Contact
+        </NavLink>
       </li>
+      
      
     </ul>
     <hr/>
     <div className="dropdown">
-      <a href="#" className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+      <div className="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
         <img src="https://avatars.githubusercontent.com/u/75838617?s=96&v=4" alt="" width="32" height="32" className="rounded-circle me-2"/>
         <strong>Nitish Sharma</strong>
-      </a>
+      </div>
       <ul className="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1" >
         <li><a className="dropdown-item" href="#">New project...</a></li>
         <li><a className="dropdown-item" href="#">Settings</a></li>
