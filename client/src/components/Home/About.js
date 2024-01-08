f919f681d411eb4cbea4877adfdedc81aa467e90
Nitish sharma <sharmanitish183@gmail.com>
2024-01-08 17:58:48 +0530

    ui almost done now fixing all the bugs

--- client/src/components/Home/About.js
+++ client/src/components/Home/About.js
@@ -5,7 +5,18 @@ export default function About() {
   return (
     <div className='d-flex'>
       <SideNav/>
-      About section
+      <div className="container bg-light vh-100 vw-100">
+      <h1>About Contact Manager</h1>
+      <p>
+        Welcome to the Contact Manager website! This is a simple application built
+        using the MERN stack (MongoDB, Express.js, React, Node.js).
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
+        Praesent libero. Sed cursus ante dapibus diam.
+      </p>
+      {/* Add more information about your application */}
+    </div>
     </div>
   )
 }
